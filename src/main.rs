13061c261ac7893f4fbc7c6c20ec522979e89dbd
FrictionlessPortals <8077147+frictionlessportals@users.noreply.github.com>
2022-10-19 17:45:19 +0100

    Fully implement authentication flow

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,16 @@ use axum::{
     routing::get,
     Extension, Router, Server,
 };
+use error::AuthError;
 use jwt::verify_token;
 use std::{env, net::SocketAddr, str::FromStr};
+use tower_cookies::{CookieManagerLayer, Cookies};
 
 // JWT Verification
 mod jwt;
+
+// Error Handling
+mod error;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -33,7 +38,9 @@ async fn main() -> Result<()> {
         .route("/", get(index))
         // `GET /auth` goes to `auth_handler`
         .route("/auth", get(auth_handler))
+        // Middleware implementations
         .layer(Extension(auth_domain))
+        .layer(CookieManagerLayer::new())
         // Fallback error handler
         .fallback(error_handler.into_service());
 
@@ -59,40 +66,39 @@ async fn index() -> impl IntoResponse {
 async fn auth_handler(
     headers: HeaderMap,
     Extension(auth_domain): Extension<String>,
-) -> impl IntoResponse {
+    cookies: Cookies,
+) -> Result<impl IntoResponse, AuthError> {
     // Attempt to extract from the given JWT header.
     let jwt_header = headers.get("Cf-Access-Jwt-Assertion");
 
     // Check if it exists, if not try the Cookie header.
-    if let Some(jwt_value) = jwt_header {
+    let claims = if let Some(jwt_value) = jwt_header {
         // JWT header does exist, extract the header value.
-        let jwt_value = match jwt_value.to_str() {
-            Ok(v) => v,
-            Err(e) => return (StatusCode::UNAUTHORIZED, e.to_string()).into_response(),
-        };
+        let jwt_value = match_error!(jwt_value.to_str(), ConvertFailure);
 
         // Validate the JWT token using the extracted header value.
-        let verification = match verify_token(jwt_value, &auth_domain) {
-            Ok(c) => c,
-            Err(e) => return (StatusCode::UNAUTHORIZED, e.to_string()).into_response(),
-        };
-
-        // Extract the user email and pass to Traefik to complete authentication.
-        (StatusCode::OK, [("X-Auth-User", verification.email)], "OK").into_response()
+        match_error!(verify_token(jwt_value, &auth_domain), VerifyFailure)
     } else {
         // JWT header doesn't exist, try using Cookie header.
-        let cookie_header = headers.get("Cookie");
-        if let Some(_cookie_value) = cookie_header {
-            (StatusCode::OK, "OK").into_response()
+        if let Some(cookie) = cookies.get("CF_Authorization") {
+            // Cookie does exist, extract the cookie value.
+            let cookie_value = cookie.value();
+
+            // Validate the JWT token using the extracted cookie value.
+            match_error!(verify_token(cookie_value, &auth_domain), VerifyFailure)
         } else {
             // Couldn't extract data for verification, return a `401`.
-            (StatusCode::UNAUTHORIZED).into_response()
+            return Err(AuthError::MissingData);
         }
-    }
+    };
+
+    // Extract the user email and pass to Traefik to complete authentication.
+    let builder = (StatusCode::OK, [("X-Auth-User", claims.email)], "OK");
+    Ok(builder.into_response())
 }
 
 /// Error handler for the application.
 async fn error_handler() -> impl IntoResponse {
     // Return with a 404 and "Not Found".
-    (StatusCode::NOT_FOUND, "Not Found")
+    (StatusCode::NOT_FOUND, "not found")
 }
